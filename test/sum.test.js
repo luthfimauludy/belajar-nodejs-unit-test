abaacad2b25a93320ce4c113a7324ff006235f40
Luthfi Putra Mauludy <putra.luthfi@gmail.com>
2022-08-11 11:21:26 +0700

    Configuration Verbose True

--- test/sum.test.js
+++ test/sum.test.js
@@ -5,3 +5,15 @@ test ("test sum function", () => {
 
     expect(result).toBe(3);
 });
+
+test ("test sum function 2", () => {
+    const result  = sum(1, 2);
+
+    expect(result).toBe(3);
+});
+
+test ("test sum function 3", () => {
+    const result  = sum(1, 2);
+
+    expect(result).toBe(3);
+});
